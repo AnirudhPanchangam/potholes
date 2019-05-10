f123b403803b0d6063d940af36e67316599ccfcf
Anirudh Panchangam <anirudhppanchangam@gmail.com>
2019-05-10 06:02:28 +0530

    changed url

--- app.py
+++ app.py
@@ -77,5 +77,5 @@ def simdata():
     return json.dumps(data)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get('PORT', 6000))
+    app.run(port=port)
