7a3180ed3decfc2b08e5731ccabbf9e971f424df
Anirudh Panchangam <anirudhppanchangam@gmail.com>
2019-05-10 05:48:43 +0530

    changed port

--- app.py
+++ app.py
@@ -77,4 +77,5 @@ def simdata():
     return json.dumps(data)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
